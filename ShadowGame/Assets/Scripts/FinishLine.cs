0004df8be5a9909f75aced15e547fa5c95361f45
SamCT <colliertenisonsa@hendrix.edu>
2025-04-17 12:46:21 -0500

    Completed Screen In, adjustments to scenes

---
+++ ShadowGame/Assets/Scripts/FinishLine.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+{
+        [SerializeField] private string newGameLevel = "";
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.CompareTag("Player")){
+            Debug.Log(collision.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+            SceneManager.LoadScene(newGameLevel);
+            
+        }
+        
+    }
+
+}
