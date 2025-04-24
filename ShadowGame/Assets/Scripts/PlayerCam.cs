397d6b83e5bf357362f17d566c50332ea13faffb
SamCT <colliertenisonsa@hendrix.edu>
2025-04-23 20:28:33 -0500

    Adjusted Camera ROV

--- ShadowGame/Assets/Scripts/PlayerCam.cs
+++ ShadowGame/Assets/Scripts/PlayerCam.cs
@@ -26,7 +26,7 @@ public class PlayerCam : MonoBehaviour
 
         yRotation += mouseX;
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f,90f);
+        xRotation = Mathf.Clamp(xRotation, -90f,40f);
 
         //Rotating the cam
         transform.rotation = Quaternion.Euler(xRotation, yRotation,0);
