1b4a6dd2305ae4637c9bac2d0b9a0b2bf6477901
Hayden Moussa <moussahj@hendrix.edu>
2025-04-28 23:11:04 -0500

    Got rid of dontdestroyonload
    caused the level to not get reset on quitting

--- ShadowGame/Assets/Scripts/GameManager.cs
+++ ShadowGame/Assets/Scripts/GameManager.cs
@@ -28,7 +28,7 @@ public class GameManager : MonoBehaviour
         else
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(gameObject);
         }
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
